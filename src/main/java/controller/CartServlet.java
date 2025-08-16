c654a99bfcddb04c77b052226afdcac31aa98c93
Santangelom3 <balotelligatto@gmail.com>
2025-08-16 09:45:06 +0200

    Ultimata logica Backend per il carrello

--- src/main/java/controller/CartServlet.java
+++ src/main/java/controller/CartServlet.java
@@ -7,27 +7,68 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import model.dao.CartDAO;
+import model.dao.CartItemDAO;
+import model.dao.ProductDAO;
+import model.dto.CartDTO;
+import model.dto.CartItemDTO;
+import model.dto.ProductDTO;
 import model.dto.UserDTO;
+import model.view.CartDispayItem;
 
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "CartServlet", value = {"/cart"})
 public class CartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession(false);
+        UserDTO user = null;
         if(session != null) {
-            UserDTO user = (UserDTO) session.getAttribute("user");
-            if (user != null) {
-                request.setAttribute("role", user.getRole());
-            }
-            else {
-                request.setAttribute("role", "Guest");
-            }
+            user = (UserDTO) session.getAttribute("user");
         }
-        else{
+
+        if(user==null) {
             request.setAttribute("role", "Guest");
+            request.setAttribute("cartItems", new ArrayList<>()); // Lista vuota
+            request.setAttribute("cartTotal", 0);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/cart.jsp");
+            dispatcher.forward(request, response);
+            return;
         }
+        request.setAttribute("role",  user.getRole());
+        DataSource dataSource = (DataSource) request.getServletContext().getAttribute("dataSource");
+        CartDAO  cartDAO = new CartDAO(dataSource);
+        CartItemDAO cartItemDAO = new CartItemDAO(dataSource);
+        ProductDAO productDAO = new ProductDAO(dataSource);
+        List<CartDispayItem> cartDispayItems = new ArrayList<>();
+        Float cartTotal = 0.0f;
+
+        try{
+            CartDTO cart = cartDAO.findByUserID(user.getId());
+
+            if(cart!=null){
+                List<CartItemDTO> cartItems = cartItemDAO.findByCartID(cart.getId());
+                for(CartItemDTO item : cartItems){
+                    ProductDTO product = productDAO.findById(item.getProductID());
+                    if(product!=null){
+                        CartDispayItem displayItem = new CartDispayItem(product,item.getQuantity());
+                        cartDispayItems.add(displayItem);
+                        cartTotal += displayItem.getSubTotal();
+
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new ServletException("Errore durante il caricamento del carrello");
+        }
+        request.setAttribute("cartItems",cartDispayItems);
+        request.setAttribute("cartTotal", cartTotal);
+
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/cart.jsp");
         dispatcher.forward(request, response);
     }
--- src/main/java/model/view/CartDispayItem.java
+++ src/main/java/model/view/CartDispayItem.java
@@ -4,5 +4,24 @@ package model.view;
 import model.dto.ProductDTO;
 import java.io.Serializable;
 
-public class CartDispayItem {
+public class CartDispayItem implements Serializable{
+    private ProductDTO product;
+    private int quantity;
+
+    public CartDispayItem(ProductDTO product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public ProductDTO getProduct() {
+        return product;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
+    public Float getSubTotal(){
+        return product.getPrice()*quantity;
+    }
 }
