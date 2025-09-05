55efac10bc2f345fe7cd1d76e1c73ec9cf708bd0
giovanni-s1919 <gianniscardone96@gmail.com>
2025-09-05 12:02:47 +0200

    creata sezione catalogo

---
+++ src/main/java/controller/CatalogServlet.java
@@ -0,0 +1,24 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "CatalogServlet", value = "/catalog")
+public class CatalogServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/jsp/catalog.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/catalog.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TechEx - Catalogo</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+        <link rel="stylesheet" href="${pageContext.request.contextPath}/css/catalog.css">
+        <link rel="icon" type="image/png" href="${pageContext.request.contextPath}/images/logo.png">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    </head>
+    <body>
+        <%@ include file="fragments/header.jsp" %>
+        <div id="main">
+
+        </div>
+        <%@ include file="fragments/footer.jsp" %>
+    </body>
+</html>
